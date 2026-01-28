e478dfd2663350cc4afcaefe444ce5c216e019a9
vmastelic <vatroslav.mastelic@gmail.com>
2026-01-28 08:42:06 +0100

    Add objects list

---
+++ script.js
@@ -0,0 +1,83 @@
+const buttons = [
+    {
+        id: "onoff",
+        label: "ON/OFF",
+        shiftLabel: null,
+        type: "action",
+        value: "power"
+    },
+
+    {
+        id: "shift",
+        label: "shift",
+        shiftLabel: null,
+        type: "action",
+        value: "shift"
+    },
+
+    {
+        id: "clear",
+        label: "clear",
+        shiftLabel: null,
+        type: "action",
+        value: "clear"
+    },
+
+    {id: "n1", label: "1", type: "number", value: 1 },
+    {id: "n2", label: "1", type: "number", value: 1 },
+    {id: "n3", label: "1", type: "number", value: 1 },
+    
+    {id: "n4", label: "1", type: "number", value: 1 },
+    {id: "n5", label: "1", type: "number", value: 1 },
+    {id: "n6", label: "1", type: "number", value: 1 },
+
+    {id: "n7", label: "1", type: "number", value: 1 },
+    {id: "n8", label: "1", type: "number", value: 1 },
+    {id: "n9", label: "1", type: "number", value: 1 },
+
+    {
+        id: "add",
+        label: "+",
+        shiftLabel: "!",
+        type: "operation",
+        value: "add",
+        shiftValue: "factorial"
+    },
+
+    {
+        id: "sub",
+        label: "-",
+        shiftLabel: "x³",
+        type: "operation",
+        value: "sub",
+        shiftValue: "cube"
+    },
+
+    {
+        id: "mul",
+        label: "x",
+        shiftLabel: "log",
+        type: "operation",
+        value: "mul",
+        shiftValue: "log"
+    },
+
+    {
+        id: "div",
+        label: "÷",
+        shiftLabel: "√",
+        type: "operation",
+        value: "div",
+        shiftValue: "sqrt"
+    },
+
+    {
+        id: "sq",
+        label: "x²",
+        shiftLabel: "³√",
+        type: "operation",
+        value: "sq",
+        shiftValue: "cuberoot"
+    }
+
+]
